947e935f232b4a3942d6ddd4b37b5ebf5c0f3b55
NqobileBuhle <139454693+NqobileBuhle@users.noreply.github.com>
2025-04-03 15:51:00 +0200

    logger

---
+++ backend/Utils/connect.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import config from "config";
+
+ async function connect(){
+    const dbUri=config.get<string>('dbUri');
+
+   try {
+    await mongoose.connect(dbUri);
+    console.log("Database connected")
+   } catch (error) {
+    console.error("Failed to connect to db");
+    process.exit(1);
+   }
+}
+export default connect;
---
+++ config/default.ts
@@ -0,0 +1,10 @@
+export default{
+    port:7107,
+    host:"localhost",
+    dbUri:"mongodb+srv://nqobilebiyela3:Ul4L3bnJqecRvBg6@cluster0.gwy9lez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    saltWorkFactor:10,
+    accessTokenTlt:"15m",
+    refreshTokenTlt:"1y",
+
+}
+
