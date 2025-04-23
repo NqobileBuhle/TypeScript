5a43be453382370848e2be1eec1c384afce729fd
NqobileBuhle <139454693+NqobileBuhle@users.noreply.github.com>
2025-04-23 11:11:07 +0200

    frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+
+  return (
+    <>
+     <div className='bg-red-200 text-white'>
+       <h1>New Project</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
